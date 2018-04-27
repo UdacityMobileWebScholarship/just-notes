fff2d7ab28b8ba17a05d2a9df383995321b23ef7
VinayV9 <vinayveeren@gmail.com>
2018-04-27 22:06:56 +0530

    controllers re created

---
+++ server/controllers/notes.js
@@ -0,0 +1,20 @@
+const Notes = require('../models/notes')
+
+const notesController = {
+    create: (req, res) => {
+        // code here
+    },
+
+    update: (req, res) => {
+
+    },
+
+    delete: (req, res) => {
+
+    },
+
+    insert: (req, res) => {
+
+    }
+}
+module.exports = notesController
