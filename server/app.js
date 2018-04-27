0aee9858844d8d0795ba188663fff05b0fc74277
VinayV9 <vinayveeren@gmail.com>
2018-04-27 18:55:15 +0530

    all set :)

--- server/app.js
+++ server/app.js
@@ -4,30 +4,26 @@ const cors = require("cors")
 const bodyParser = require("body-parser")
 const app = express()
  
-
 //parsers
 app.use(cors())
 app.use(morgan("dev"))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-
-
 // server client files
-app.use(express.static(__dirname + "/../client/dist"))
+app.use(express.static(__dirname + "/../client/build"))
 app.get('/', (req, res)=>{
-   res.sendFile('/../client/dist/index.html')
+   res.sendFile('/../client/build/index.html')
 })
 
-
+// redirector
 app.get('*', (req, res) => {
   res.redirect('/')
 })
 
 
-
 //routes
-app.use("/notes", require("./api/routes/"))
+app.use("/notes", require("./controllers/notes.controller"))
 
 
 module.exports = app
