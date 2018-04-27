0dc509e95f6add9d08406972e085b2bcf6591e29
VinayV9 <vinayveeren@gmail.com>
2018-04-27 22:09:51 +0530

    indentation  cleared

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 const app = require("./app")
 const port = process.env.PORT || 8080
 
-
 // start server
 const server = app.listen(port, function () {
     console.log(`Server listening on port ${port}`)
