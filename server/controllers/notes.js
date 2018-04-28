efe3583a07a835f8c0ac3c54056866704a91dc39
Anurag Majumdar <an.mostwanted@gmail.com>
2018-04-28 22:21:47 +0530

    Add index and create routes

--- server/controllers/notes.js
+++ server/controllers/notes.js
@@ -1,30 +1,66 @@
-const Notes = require('../models/notes')
+const Note = require('../models/notes');
 
 const notesController = {
     create: (req, res) => {
-        // code here
-        console.log(`create: ${req.body}`)
-        res.send('ok')
+        const note = new Note({
+            title: req.body.title,
+            notes: req.body.notes
+        });
+
+        note.save()
+            .then(note => {
+                return res.status(200).json({
+                    error: false,
+                    message: 'Note created',
+                    note: {
+                        _id: note._id,
+                        title: note.title,
+                        notes: note.notes
+                    }
+                });
+            })
+            .catch(error => {
+                return res.status(500).json({
+                    error: true,
+                    message: 'Error Creating A Note!'
+                });
+            });
+
+        console.log(`create: ${req.body}`);
     },
 
     update: (req, res) => {
-        console.log(`update: ${req.params}`)
-        res.send('ok')
+        console.log(`update: ${req.params}`);
+        res.send('ok');
     },
 
     delete: (req, res) => {
-        console.log(`delete: ${req.params}`)
-        res.send('ok')
+        console.log(`delete: ${req.params}`);
+        res.send('ok');
     },
 
     index: (req, res) => {
-       console.log(`index: ${req.body}`)
-       res.send('ok')
+        Note.find()
+            .exec()
+            .then(notes => {
+                return res.status(200).json({
+                    error: false,
+                    notes: notes
+                });
+            })
+            .catch(error => {
+                return res.status(500).json({
+                    error: true,
+                    message: 'Error Fetching Notes!'
+                });
+            });
+        console.log(`index: ${req.body}`);
     },
 
     get: (req, res) => {
-        console.log(`get: ${req.params}`)
-        res.send('ok')
+        console.log(`get: ${req.params}`);
+        res.send('ok');
     }
 }
-module.exports = notesController
+
+module.exports = notesController;
