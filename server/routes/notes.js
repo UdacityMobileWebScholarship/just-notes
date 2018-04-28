e511b4e8bb1ae595e85fae3b128cc0eaed793fe0
VinayV9 <vinayveeren@gmail.com>
2018-04-28 13:24:20 +0530

    api testing with postman

--- server/routes/notes.js
+++ server/routes/notes.js
@@ -5,7 +5,7 @@ const notesController = require('../controllers/notes')
 //crud operations
 router.post('/notes', notesController.create)
 router.delete('/notes/:id', notesController.delete)
-router.update('/notes/:id', notesController.update)
+router.put('/notes/:id', notesController.update)
 router.get('/notes/:id', notesController.get)
 router.get('/notes', notesController.index)
 //TODO getting all notes
