af582671a48561876f12040c922559003daa68b7
VinayV9 <vinayveeren@gmail.com>
2018-04-30 14:56:26 +0530

    handled 3 cases

--- server/controllers/notes.js
+++ server/controllers/notes.js
@@ -37,19 +37,11 @@ const notesController = {
         Note.find({ deleted: false })
             .exec()
             .then(notes => {
-                if(notes){
-                    return res.status(200).json({
-                        error: false,
-                        notes: notes,
-                        message:"Records found!"
-                    })
-                }else{
-                    return res.status(200).json({
-                        error: false,
-                        notes: {},
-                        message:"No records found!"
-                    })
-                }
+                notes = notes || []
+                return res.status(200).json({
+                    error: false,
+                    notes: notes, 
+                }) 
             })
             .catch(error => {
                 return res.status(500).json({
