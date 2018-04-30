0ba9c84cabddad3951a9b24cdddc101204627971
Manajit Pal <hello@manajitpal.online>
2018-05-01 02:10:23 +0530

    UI changes and fixes of Home Screen

--- src/components/NoteCard/noteCard.js
+++ src/components/NoteCard/noteCard.js
@@ -5,7 +5,25 @@ class NoteCard extends Component {
   render() {
     return (
       <div className="noteContainer">
-        <span>Note card here</span>
+        <div className="noteContentTop">
+          <div>
+            <a href="" title="Share this note">
+              <i className="fas fa-share-square" />
+            </a>
+            <a href="" title="Download this note">
+              <i className="fas fa-download" />
+            </a>
+            <a href="" title="Delete this note">
+              <i className="fas fa-trash" />
+            </a>
+          </div>
+          <span>Date</span>
+        </div>
+        <h2>Title</h2>
+        <p>
+          I am some paragraph<br />
+          I am line two of the paragraph
+        </p>
       </div>
     );
   }
