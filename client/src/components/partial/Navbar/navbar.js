2f79d8efadf6ea8f5b2180d0b1f22f2001c24058
Ayan Dey <ayand04@gmail.com>
2018-05-04 10:31:02 +0530

    Added searchfield in mobile view

--- client/src/components/partial/Navbar/navbar.js
+++ client/src/components/partial/Navbar/navbar.js
@@ -2,6 +2,46 @@ import React, { Component } from "react";
 import "./navbar.css";
 
 class Navbar extends Component {
+  state = {
+    searchVisible: false
+  };
+
+  isMobile = () => {
+    const width = window.innerWidth;
+
+    if (width >= 992) {
+      this.setState({ searchVisible: true });
+      return false;
+    } else {
+      this.setState({ searchVisible: false });
+      return true;
+    }
+  };
+
+  onSearchButtonClick = event => {
+    if (this.isMobile()) {
+      if (this.state.searchVisible) {
+        this.setState({ searchVisible: false });
+      } else {
+        this.setState({ searchVisible: true });
+      }
+    } else {
+      event.preventDefault();
+    }
+  };
+
+  componentWillMount() {
+    this.isMobile();
+  }
+
+  componentDidMount() {
+    window.addEventListener("resize", this.isMobile);
+  }
+
+  componentWillUnmount() {
+    window.addEventListener("resize", this.isMobile);
+  }
+
   render() {
     return (
       <div id="navContainer">
@@ -10,10 +50,12 @@ class Navbar extends Component {
         </div>
         <div id="searchbar">
           <div id="searchForm">
-            <div id="searchIcon">
+            <div id="searchIcon" onClick={this.onSearchButtonClick}>
               <i className="fas fa-search fa-lg" />
             </div>
-            <input type="text" placeholder="Search" />
+            {this.state.searchVisible ? (
+              <input type="text" placeholder="Search" />
+            ) : null}
           </div>
           <button id="addNoteBtn" type="button">
             <i className="fas fa-plus fa-lg" />
