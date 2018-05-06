3e566c68d0caa4fba8500ca6ad04d5723f6bb590
Manajit Pal <hello@manajitpal.online>
2018-05-06 23:11:01 +0530

    adding hyperlink to brand

--- client/src/components/partial/Navbar/navbar.js
+++ client/src/components/partial/Navbar/navbar.js
@@ -59,7 +59,9 @@ class Navbar extends Component {
     return (
       <nav id="navContainer">
         <div id="brand">
+        <a href="/">
           <span>Just Notes</span>
+        </a>
         </div>
         <div id="searchbar">
           <div id="searchForm">
