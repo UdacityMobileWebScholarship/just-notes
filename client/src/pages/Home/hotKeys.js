d8cd53540596024a38de9ebd2a7cd12344796cdf
suyash bhutani <suyashsidzzz@gmail.com>
2018-05-12 16:19:07 +0530

    hide hotekeys option which r yet to be added

--- client/src/pages/Home/hotKeys.js
+++ client/src/pages/Home/hotKeys.js
@@ -1,21 +1,21 @@
 const hotKeysLst = {
-  editor: [
-    {
-      name: "Bold",
-      key: "alt/option + b",
-      desc: "Make the text bold"
-    },
-    {
-      name: "Italic",
-      key: "alt/option + i",
-      desc: "Make the text italic"
-    },
-    {
-      name: "strikethrough",
-      key: "alt/option + s",
-      desc: "Make the text strikethrough"
-    }
-  ],
+  // editor: [
+  //   {
+  //     name: "Bold",
+  //     key: "alt/option + b",
+  //     desc: "Make the text bold"
+  //   },
+  //   {
+  //     name: "Italic",
+  //     key: "alt/option + i",
+  //     desc: "Make the text italic"
+  //   },
+  //   {
+  //     name: "strikethrough",
+  //     key: "alt/option + s",
+  //     desc: "Make the text strikethrough"
+  //   }
+  // ],
   general: [
     {
       name: "Search",
