668e253b0ff58e4609b59c6ec8e6c2e596e13f32
Manajit Pal <hello@manajitpal.online>
2018-05-12 16:06:43 +0530

    Formatting styles

--- client/src/pages/Home/hotKeys.js
+++ client/src/pages/Home/hotKeys.js
@@ -2,29 +2,29 @@ const hotKeysLst = {
   editor: [
     {
       name: "Bold",
-      key: "alt + b",
+      key: "alt/option + b",
       desc: "Make the text bold"
     },
     {
       name: "Italic",
-      key: "alt + i",
+      key: "alt/option + i",
       desc: "Make the text italic"
     },
     {
       name: "strikethrough",
-      key: "alt + s",
+      key: "alt/option + s",
       desc: "Make the text strikethrough"
     }
   ],
   general: [
     {
       name: "Search",
-      key: "alt + l",
+      key: "alt/option + l",
       desc: "Focus on search bar"
     },
     {
       name: "Toggle Theme",
-      key: "alt + t",
+      key: "alt/option + t",
       desc: "Toggle theme (Dark/light)"
     }
   ]
