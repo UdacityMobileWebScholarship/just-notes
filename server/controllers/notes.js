04a536659327898f07d4ce793955197d0a552ffc
Istiaque Siddiqi <istiaquesvst@IN.nam.ad.pwcinternal.com>
2018-05-14 13:28:02 +0530

    added api for upadate, delete, and get a single note

--- server/controllers/notes.js
+++ server/controllers/notes.js
@@ -24,13 +24,59 @@ const notesController = {
     },
 
     update: (req, res) => {
-        console.log(`update: ${req.params}`)
-        res.send('ok')
+        // console.log(`update: ${req.params}`)
+        // res.send('ok')
+        Note.findOneAndUpdate({slug: req.params.id}, {
+            title: req.body.title, 
+            notes: req.body.notes, 
+            updated_at: Date.now()
+        }, {new: true})
+        .then(note => {
+            if(!note) {
+                return res.status(404).json({
+                    error: false,
+                    message: "Note not found with id " + req.params.id,
+                });
+            }
+            return res.status(200).json({
+                error: false,
+                notes: note, 
+            });
+        }).catch(err => {
+            return res.status(500).json({
+                error: true,
+                message: 'Error Fetching Notes!'
+            });
+        });
     },
 
     delete: (req, res) => {
-        console.log(`delete: ${req.params}`)
-        res.send('ok')
+        // console.log(`delete: ${req.params}`)
+        // res.send('ok')
+        Note.findOneAndRemove({slug: req.params.id})
+        .then(note => {
+            if(!note) {
+                return res.status(404).json({
+                    error: false,
+                    message: "Note not found with id " + req.params.id,
+                });            
+            }
+            return res.status(200).json({
+                error: false,
+                message: "Note deleted successfully!", 
+            });
+        }).catch(err => {
+            if(err.kind === 'ObjectId' || err.name === 'NotFound') {
+                return res.status(404).json({
+                    error: false,
+                    message: "Note not found with id " + req.params.id,
+                });                  
+            }
+            return res.status(500).json({
+                error: true,
+                message: "Could not delete note with id " + req.params.id,
+            });
+        });
     },
 
     index: (req, res) => {
@@ -52,8 +98,26 @@ const notesController = {
     },
 
     get: (req, res) => {
-        console.log(`get: ${req.params}`)
-        res.send('ok')
+        console.log(`get: ${req.params.id}`)
+        // res.send('ok')
+        Note.findOne({slug: req.params.id})
+        .then(note => {
+            if(!note) {
+                return res.status(404).json({
+                    error: false,
+                    message: "Note not found with id " + req.params.id,
+                });            
+            }
+            return res.status(200).json({
+                error: false,
+                notes: note, 
+            });
+        }).catch(err => {
+            return res.status(500).json({
+                error: true,
+                message: 'Error Fetching Notes!'
+            });
+        });
     }
 }
 
